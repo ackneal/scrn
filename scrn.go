93325fb9e40783ef4f478e0b5032a3afe0bf23e3
NealH <neal.anglo@gmail.com>
2020-04-03 21:04:41 +0800

    feat: add Screen print methods

--- scrn.go
+++ scrn.go
@@ -22,3 +22,12 @@ func (s *Screen) Clear() {
 func (s *Screen) Move(col, row int) {
         os.Stdout.WriteString(fmt.Sprintf("\033[%d;%dH", row, col))
 }
+
+func (s *Screen) Print(text string) (n int, err error) {
+        return os.Stdout.WriteString(text)
+}
+
+func (s *Screen) Printf(format string, a ...interface{}) (n int, err error) {
+        text := fmt.Sprintf(format, a...)
+        return s.Print(text)
+}
